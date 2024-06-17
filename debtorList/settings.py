7c40531fadcd5ca25886c5bba377b099597f9c28
pravison <pravisojuma087@gmail.com>
2024-06-17 10:12:00 +0300

    second commit

--- debtorList/settings.py
+++ debtorList/settings.py
@@ -83,13 +83,22 @@ WSGI_APPLICATION = 'debtorList.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
+import dj_database_url 
+
+DATABASES = {
+    'default': dj_database_url.parse(
+        env('POSTGRES_URL'),
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
