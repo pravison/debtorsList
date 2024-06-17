c33e8b63c89509eb8d42090dc1611e45f6979fbf
pravison <pravisojuma087@gmail.com>
2024-06-17 18:06:42 +0300

    fifth commit

--- debtorList/wsgi.py
+++ debtorList/wsgi.py
@@ -8,12 +8,11 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
 import os
-import environ
-
-env = environ.Env()
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'debtorList.settings')
 
 application = get_wsgi_application()
+
+app = application
